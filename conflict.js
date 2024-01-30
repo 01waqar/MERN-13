30de2dc63d9768012f74a45a9d30b1e1341302a9
01waqar <waqarahsan125@gmail.com>
2024-01-30 19:03:33 +0500

    This the 2 branch child branch with some lines of code

--- conflict.js
+++ conflict.js
@@ -1,3 +1,9 @@
+const = (function/test2) =>{
+    console.log("check main");
+    console.log("function test1");
+    console.log("function test2");
+};
+
 const = () =>{
     console.log("check main")
 };
